550e1edf67aa3817c0806324d6e7121c7cb3b44a
rafaela yazawa <yazawrafa@gmail.com>
2025-07-07 21:58:16 +0900

    finished exercise 2.18 and 2.19, added the weather key but haven't started the exercise

--- part-2/countries-data/src/App.jsx
+++ part-2/countries-data/src/App.jsx
@@ -4,13 +4,17 @@ import React, { useEffect, useState } from "react";
 const CountryDetails = ({ country }) => {
   return (
     <div>
-      <h2>{country.name}</h2>
-      <p>Capital: {country.capital}</p>
-      <p>Area - {country.area}</p>
-      <h2>Lamguages</h2>
+      <h1>{country.name.common}</h1>
+      <p>Capital: {country.capital[0]}</p>
+      <p>Area - {country.area} km²</p>
+      <h2>Languages</h2>
       <ul>
-        <li>{country.languages}</li>
+        {Object.values(country.languages).map((lang) => (
+          <li key={lang}>{lang}</li>
+        ))}
       </ul>
+
+      <img src={country.flags.png} alt={country.flags.alt} />
     </div>
   );
 };
@@ -19,6 +23,7 @@ const App = () => {
   const apiUrl = "https://studies.cs.helsinki.fi/restcountries/api";
   const [searchTerm, setSearchTerm] = useState("");
   const [allCountries, setAllCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
   useEffect(() => {
     axios
@@ -35,6 +40,9 @@ const App = () => {
   const filteredCountries = allCountries.filter((country) =>
     country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  // console.log("filteredCountries", filteredCountries);
+  // console.log("filteredCountries[0]", filteredCountries[0]);
 
   const handleInputChange = (e) => {
     setSearchTerm(e.target.value);
@@ -56,13 +64,19 @@ const App = () => {
             <ul>
               {searchTerm &&
                 filteredCountries.map((country) => (
-                  <li key={country.name.common}>{country.name.common}</li>
+                  <div key={country.name.common}>
+                    <li key={country.name.common}>{country.name.common}</li>
+                    <button onClick={() => setSelectedCountry(country)}>
+                      Show
+                    </button>
+                  </div>
                 ))}
             </ul>
           )}
+        {selectedCountry && <CountryDetails country={selectedCountry} />}
 
         {searchTerm && filteredCountries.length === 1 && (
-          <CountryDetails country={filteredCountries} />
+          <CountryDetails country={filteredCountries[0]} />
         )}
       </div>
     </div>
