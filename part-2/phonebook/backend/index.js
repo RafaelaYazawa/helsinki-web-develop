48d4accbecd1b36063a6c105c3e95849e41ffa50
rafaela yazawa <yazawrafa@gmail.com>
2025-07-31 22:30:49 +0900

    I finished the exercises 3.1 and 3.2

--- part-2/phonebook/backend/index.js
+++ part-2/phonebook/backend/index.js
@@ -1,2 +1,52 @@
 const express = require("express");
 const app = express();
+
+persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} persons</p>
+    <p> ${new Date()}</p>`
+  );
+});
+
+app.get("/api/persons", (request, response) => {
+  console.log("person", persons);
+  response.json(persons);
+});
+
+const generateId = () => {
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0;
+
+  return String(maxId + 1);
+};
+
+app.get("/api/persons/:id", (request, response) => {});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
