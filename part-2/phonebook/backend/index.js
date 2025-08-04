26c243c656b0c1de57e0a82321a51934b04c7d4c
rafaela yazawa <yazawrafa@gmail.com>
2025-08-04 19:04:55 +0900

    completed exercise 3.7 in part 3 from node js and express

--- part-2/phonebook/backend/index.js
+++ part-2/phonebook/backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
-persons = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
