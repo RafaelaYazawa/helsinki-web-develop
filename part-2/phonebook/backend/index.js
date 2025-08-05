97fc15acdcaff362e1789169d822919742dbc2f5
rafaela yazawa <yazawrafa@gmail.com>
2025-08-05 16:45:47 +0900

    deployed the backend in render, and connected the backend and the frontend for the production mode

--- part-2/phonebook/backend/index.js
+++ part-2/phonebook/backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 app.use(express.json());
+app.use(express.static("dist"));
 morgan.token("body", (req, res) => {
   return JSON.stringify(req.body);
 });
